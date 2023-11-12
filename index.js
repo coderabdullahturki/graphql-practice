5d707fe1a2ed634ececa9d61b78cf50f1e371d30
Abdullah Turky <abdullahturki087@gmail.com>
2023-11-13 00:09:11 +0600

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Define your GraphQL schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// Define your resolvers
+const root = {
+  hello: () => 'Hello, GraphQL World!',
+};
+
+const app = express();
+
+// Use the express-graphql middleware
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true, // Enable the GraphiQL interface for easy testing
+}));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/graphql`);
+});
